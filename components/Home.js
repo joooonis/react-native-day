af8c67c1f91174751f35222ede7be520c1584430
jooooonis <xowns3213@naver.com>
2022-10-23 16:59:51 +0900

    feat : fetch data

--- components/Home.js
+++ components/Home.js
@@ -1,21 +1,41 @@
 import { Text, View, TouchableOpacity } from 'react-native';
 import styles from '../styles';
 
+const url = 'http://qa2.ringleserver.com/api/v4/student/landing/webinar';
+
+import axios from 'axios';
+import { useState } from 'react';
+
 function Home({ navigation }) {
+  const [webinars, setWebinars] = useState([]);
+
+  useEffect(() => {
+    // http requset
+    axios
+      .get(url)
+      .then(function (response) {
+        // handle success
+        const { success, webinars: _webinars } = response.data;
+
+        console.log(_webinars);
+        if (success) {
+          setWebinars(_webinars);
+        }
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          console.log('hi');
-        }}>
-        <Text>Home</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('Detail');
-        }}>
-        <Text>Go Detal</Text>
-      </TouchableOpacity>
+      <Text>webinars[0].title</Text>
+      <Text>webinars[0].title</Text>
+      <Text>webinars[0].title</Text>
     </View>
   );
 }
